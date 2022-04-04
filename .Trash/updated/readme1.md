b24c73d21a7377c20529296809bdb3202502bdae
dcat <36656965+angelaunique@users.noreply.github.com>
2022-04-04 15:20:36 -0700

    Delete .Trash directory
    delete repete repo

--- .Trash/updated/readme1.md
+++
@@ -1,1 +0,0 @@
-This project is practice coding follow udemy course: react-complete-guide by Maximilian Schwarzmuller
--- .Trash/updated/src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import "./Card.css";
-import React from "react";
-const Card = (props) => {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
-export default Card;
-//props.children 是为了create props
